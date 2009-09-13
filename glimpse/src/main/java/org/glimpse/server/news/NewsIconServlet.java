d722fd0964042ec72f2394e73ce6f8195d3920a9
nicolas <nicolas@efd028e8-07b5-41f1-8d32-573794bb6f74>
2009-09-13 07:34:32 +0000

    git-svn-id: https://epsilon.dutertry.com/svn/nicolas/glimpse/trunk@30 efd028e8-07b5-41f1-8d32-573794bb6f74

--- glimpse/src/main/java/org/glimpse/server/news/NewsIconServlet.java
+++ glimpse/src/main/java/org/glimpse/server/news/NewsIconServlet.java
@@ -3,7 +3,6 @@ package org.glimpse.server.news;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.URL;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
