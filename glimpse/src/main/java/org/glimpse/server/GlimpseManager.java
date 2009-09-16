8a0707bacbd60ebe23021a97423d272d7ea4d9da
nicolas <nicolas@efd028e8-07b5-41f1-8d32-573794bb6f74>
2009-09-16 17:32:07 +0000

    git-svn-id: https://epsilon.dutertry.com/svn/nicolas/glimpse/trunk@39 efd028e8-07b5-41f1-8d32-573794bb6f74

--- glimpse/src/main/java/org/glimpse/server/GlimpseManager.java
+++ glimpse/src/main/java/org/glimpse/server/GlimpseManager.java
@@ -9,29 +9,30 @@ import javax.servlet.ServletContextListener;
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.PropertyConfigurator;
 
 public class GlimpseManager implements ServletContextListener {
-	private static final Log logger = LogFactory.getLog(GlimpseManager.class);
-	
 	private Configuration configuration;
 	private ConnectionManager connectionManager;
 
 	public void contextInitialized(ServletContextEvent sce) {
 		try {
 			ServletContext context = sce.getServletContext();			
-			context.setAttribute("org.glimpse.manager", this);			
+			context.setAttribute("org.glimpse.manager", this);
 			
 			String confDir = System.getProperty("org.glimpse.conf.dir");
 			if(StringUtils.isEmpty(confDir)) {
 				confDir = context.getRealPath("/WEB-INF/conf");
 			}
+			
+			File logConfig = new File(confDir, "log4j.properties");
+			PropertyConfigurator.configure(logConfig.getAbsolutePath());
+			
 			configuration = new PropertiesConfiguration(new File(confDir,
 					"glimpse.properties"));
 			connectionManager = new SimpleConnectionManager();
 		} catch (Exception e) {
-			logger.fatal("Error", e);
+			e.printStackTrace();
 		}
 
 	}
