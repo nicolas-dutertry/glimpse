beef75dc34345c9cbd7b7d13ea1a7483555040da
nicolas <nicolas@efd028e8-07b5-41f1-8d32-573794bb6f74>
2010-02-22 16:51:24 +0000

    git-svn-id: https://epsilon.dutertry.com/svn/nicolas/glimpse/trunk@74 efd028e8-07b5-41f1-8d32-573794bb6f74

---
+++ glimpse-dev-utils/src/main/java/org/glimpse/dev/JettyLauncher.java
@@ -0,0 +1,50 @@
+/* *******************************************************
+ * © 1996-2009 HR Access Solutions. All rights reserved
+ * ******************************************************/
+
+package org.glimpse.dev;
+
+import java.io.File;
+
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.webapp.WebAppContext;
+
+
+public class JettyLauncher {
+
+	public static void main(String[] args) {
+		try {
+			if (args.length != 3) {
+				printUsage();
+				return;
+			}
+			String webAppPath = args[0];
+			String appName = args[1];
+
+			Server jetty = new Server(Integer.parseInt(args[2]));
+			WebAppContext context = new WebAppContext(webAppPath, "/" + appName);
+
+			File jettyDir = new File(new File(System.getProperty("java.io.tmpdir")), "jetty");
+			File appDir = new File(jettyDir, appName);
+			appDir.mkdirs();
+
+			context.setTempDirectory(appDir);
+
+			jetty.addHandler(context);
+			jetty.start();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void printUsage() {
+		System.out
+				.println("Usage : org.glimpse.dev.JettyLauncher webappDir wabappName port");
+		System.out
+				.println("   - webappDir : path to the directory containing the web application");
+		System.out
+				.println("   - webappName : web application name (context path)");
+		System.out.println("   - port : listening port");
+	}
+
+}
