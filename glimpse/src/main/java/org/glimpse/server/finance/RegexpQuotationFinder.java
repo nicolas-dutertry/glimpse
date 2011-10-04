b5afa873b387610cc04f76749a07308f081d6fe0
nicolas <nicolas@efd028e8-07b5-41f1-8d32-573794bb6f74>
2011-10-04 13:54:24 +0000

    Financial regexp change
    git-svn-id: https://epsilon.dutertry.com/svn/nicolas/glimpse/trunk@128 efd028e8-07b5-41f1-8d32-573794bb6f74

--- glimpse/src/main/java/org/glimpse/server/finance/RegexpQuotationFinder.java
+++ glimpse/src/main/java/org/glimpse/server/finance/RegexpQuotationFinder.java
@@ -9,7 +9,7 @@ import org.glimpse.client.finance.Quotation;
 public class RegexpQuotationFinder implements QuotationFinder {
 	private Pattern pattern =
 		Pattern.compile(
-				"<div class=\"InfB\">\\s*<span class=\"gras\">\\s*([0-9\\s\\.]+)\\s*(\\(c\\))?\\s*(\\w*)\\s*</span>(&nbsp;)*\\s*<span\\s+class=\"gras\">\\s*<span class=\"VAR[a-z]*\">([-\\+]?[0-9\\s\\.]+)%</span>");
+				"<big class=\"fv-last\">\\s*<span .*>([0-9\\s\\.]+)\\s*(\\(c\\))?\\s*(\\w*)\\s*</span></span>\\s*</big>\\s*(&nbsp;)*\\s*<big class=\"fv-var\">\\s*<span .*>([-\\+]?[0-9\\s\\.]+)%</span>");
 	@Override
 	public Quotation getQuotation(String text) {
 		Matcher matcher = pattern.matcher(text);
